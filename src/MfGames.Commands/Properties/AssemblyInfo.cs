d75021cff689160b5c355187d03d84295a500b23
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2013-06-20 21:06:47 -0500

    Initial version of the code.
    - Includes a NuGet package for 0.1.0.
    - includes both the generic commands and a text-editor specific set of commands.

---
+++ src/MfGames.Commands/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2011-2013 Moonfire Games
+// Released under the MIT license
+// http://mfgames.com/mfgames-gtkext-cil/license
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("MfGames.Commands")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Moonfire Games")]
+[assembly: AssemblyProduct("MfGames.Commands")]
+[assembly: AssemblyCopyright("Copyright © 2013, Moonfire Games")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("8e27e5e0-09f8-4549-af59-5f6b749dc467")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
